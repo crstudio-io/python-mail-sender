57520f1dda7ea8b1c43dd6ccddf50558487247d8
aquashdw <aquashdw@gmail.com>
2024-06-08 22:33:20 +0900

    MIMEMultipart

--- sender.py
+++ sender.py
@@ -2,17 +2,19 @@ import os
 import smtplib
 
 from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 subject = "test subject"
-body = "Email Body"
+body = "<h1>Hello</h1>"
 sender = "aquashdw@gmail.com"
 recipients = ["aquashdw@gmail.com"]
 password = os.getenv("GMAIL_PASSKEY")
 
-msg = MIMEText(body)
+msg = MIMEMultipart('alternative')
 msg["Subject"] = subject
-msg['From'] = sender
-msg['To'] = ', '.join(recipients)
-with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp_server:
+msg["From"] = sender
+msg["To"] = ", ".join(recipients)
+msg.attach(MIMEText(body, "html"))
+with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp_server:
     smtp_server.login(sender, password)
     smtp_server.sendmail(sender, recipients, msg.as_string())
